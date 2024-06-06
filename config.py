92572e453348f83fb351544fe59c42b07eefa098
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2024-06-06 22:21:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DB_URI = "mongodb+srv://haris12:harisa 12@cluster0.u8qzvr4.mongodb.net/?retryWri
 OWNER_ID = 614393705
 LOGGER_ID = "-1001457998546"
 SUDO_USER = 614393705
-LOGGER_LEVEL = "LOGGER_LEVEL", 20
+LOGGER_LEVEL = 20
 "logger level, `debug(10)`, `info(20)`, `warn(30)` and `error(40)`. default is `info`"
 
 TIME_ZONE = "Asia/Kolkata"
